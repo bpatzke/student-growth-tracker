3c54c302d0df56282e6d25f42879bfd23802b284
Bryan Patzke <projects@2bpencil.com>
2015-12-10 06:36:07 -0600

    Initial updates for migration to mysql database.

---
+++ private/db/create-db-student-growth-tracker.sql
@@ -0,0 +1,4 @@
+delimiter $$
+
+CREATE DATABASE `student_growth_tracker` /*!40100 DEFAULT CHARACTER SET utf8 */$$
+
---
+++ private/db/drop_tables.sql
@@ -0,0 +1,21 @@
+use student_growth_tracker;
+
+drop table if exists student_grade;
+drop table if exists student_classes;
+drop table if exists class_grade;
+drop table if exists grade_standard;
+drop table if exists standard;
+drop table if exists student;
+drop table if exists gradebook;
+drop table if exists classes;
+drop table if exists grade;
+drop table if exists grade_type;
+drop table if exists contentarea;
+drop table if exists school_member;
+drop table if exists school;
+drop table if exists auth_permission;
+drop table if exists auth_event;
+drop table if exists auth_cas;
+drop table if exists auth_membership;
+drop table if exists auth_group;
+drop table if exists auth_user;
